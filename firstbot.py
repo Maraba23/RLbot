759b5a9c16fe800bf78956973e24d6c55448c692
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2024-03-04 13:39:44 -0300

    Começando novamente

---
+++ firstbot.py
@@ -0,0 +1,14 @@
+import rlgym
+from stable_baselines3 import PPO
+from rlgym.utils.reward_functions.common_rewards import LiuDistancePlayerToBallReward, VelocityBallToGoalReward
+
+# Create the environment
+env = rlgym.make("default", self_play=True, spawn_opponents=False, team_size=1, tick_skip=8, reward_fn=[LiuDistancePlayerToBallReward(), VelocityBallToGoalReward()])
+
+# Train the agent
+model = PPO("MlpPolicy", env, verbose=1)
+
+model.learn(total_timesteps=100000)
+
+# Save the agent
+model.save("models/ppo_default")
